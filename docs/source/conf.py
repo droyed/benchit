bd67f90710021c56fec1b28165aab450f8291b92
Divakar Roy <droygatech@gmail.com>
2020-04-10 04:19:46 +0530

    fix for apidocs bug

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,10 +12,13 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
-
 
 # -- Project information -----------------------------------------------------
 
@@ -74,8 +77,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'alabaster'
-html_theme = "sphinx_rtd_theme"
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -173,7 +175,11 @@ epub_title = project
 # A list of files that should not be packed into the epub file.
 epub_exclude_files = ['search.html']
 
-extensions = ['sphinx.ext.napoleon']
+
+html_theme = "sphinx_rtd_theme"
+
+extensions = ['sphinx.ext.napoleon', 'sphinx.ext.autodoc']
 napoleon_google_docstring = False
 napoleon_use_param = False
 napoleon_use_ivar = True
+
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,5 +1,5 @@
 .. benchit documentation master file, created by
-   sphinx-quickstart on Fri Apr 10 03:58:02 2020.
+   sphinx-quickstart on Fri Apr 10 04:17:36 2020.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
