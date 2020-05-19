8afd638b0f9f1107803bd34db4a0d4cd6bf929cc
Divakar Roy <droygatech@gmail.com>
2020-05-19 22:58:39 +0530

    v002 changes docs fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,7 @@ release = u'0.0.2'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.napoleon', 'sphinx.ext.autodoc']
+extensions = ['sphinx.ext.napoleon', 'sphinx.ext.autodoc', 'sphinx.ext.autosectionlabel']
 
 
 # Add any paths that contain templates here, relative to this directory.
