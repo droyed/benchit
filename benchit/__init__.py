15cdbd65e20a2b941a04d0b52a442b9673f4c1cb
droyed <droygatech@gmail.com>
2020-10-15 22:23:17 +0530

    v005 start commit

--- benchit/__init__.py
+++ benchit/__init__.py
@@ -1,6 +1,6 @@
 from benchit.main import BenchmarkObj, timings, bench, set_environ
 from benchit.utils import extract_modules_from_globals, print_specs
 
-__version__ = '0.0.4'
+__version__ = '0.0.5'
 
 __all__ = ["BenchmarkObj", "bench", "timings", "extract_modules_from_globals", "print_specs", "set_environ"]
